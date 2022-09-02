1418529ca871e6ac2300323a28ba3f2136555db0
angelinperezz <angelinperezpacheco.19@gmail.com>
2022-09-02 13:56:54 -0400

    PreMainActivity Listo
    Xml con Java.class Juntos

--- app/src/main/java/com/example/directoryapp/PreMainActivity.java
+++ app/src/main/java/com/example/directoryapp/PreMainActivity.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -23,6 +24,7 @@ public class PreMainActivity extends AppCompatActivity {
     public static List<User> usersSQLite = new ArrayList<>();
     public static List<User> usersRetrofit = new ArrayList();
     private static Context context;
+    private Button botonIngresar;
 
 
     @Override
@@ -37,15 +39,8 @@ public class PreMainActivity extends AppCompatActivity {
         botonIngresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (UtilsNetwork.isOnline(PreMainActivity.this)) {
                     Intent intent = new Intent(PreMainActivity.this, MainActivity.class);
-
                     startActivity(intent);
-
-                } else {
-
-                    Toast.makeText(PreMainActivity.this, "Necesitas internet para crear una tarjeta.", Toast.LENGTH_LONG).show();
-                }
 
             }
         });
